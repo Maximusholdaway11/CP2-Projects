1d47818a05b31286a6dea231492e55d1323f5880
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-01-29 19:18:33 +0000

    Made the morse and english lists.

---
+++ simple_morse_code_translator/main.py
@@ -0,0 +1,12 @@
+#Max Holdaway Simple Morse Code Translator
+
+alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+morse_characters_list = [". ---", "--- . . .", "--- . --- .", "--- . .", ".", ". . --- .", "--- --- .", ". . . .", ". .", ". --- --- ---", "--- . ---", ". --- . .", "--- ---", "--- .", "--- --- ---", ". --- --- .", "--- --- . ---", ". --- .", ". . .", "---", ". . ---", ". . . ---", ". --- ---", "--- . . ---", "--- . --- ---", "--- --- . ."]
+morse_letter_space = "   "
+morse_word_space = "       "
+
+def english_to_morse():
+    pass
+
+def morse_to_english():
+    pass
