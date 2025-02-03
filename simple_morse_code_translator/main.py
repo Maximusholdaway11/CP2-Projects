8c5d189f18935ac582eb4925c01a2f74f8d9894d
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-03 00:16:16 +0000

    Made the english to morse code part of this project.

--- simple_morse_code_translator/main.py
+++ simple_morse_code_translator/main.py
@@ -1,12 +1,32 @@
 #Max Holdaway Simple Morse Code Translator
 
-alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-morse_characters_list = [". ---", "--- . . .", "--- . --- .", "--- . .", ".", ". . --- .", "--- --- .", ". . . .", ". .", ". --- --- ---", "--- . ---", ". --- . .", "--- ---", "--- .", "--- --- ---", ". --- --- .", "--- --- . ---", ". --- .", ". . .", "---", ". . ---", ". . . ---", ". --- ---", "--- . . ---", "--- . --- ---", "--- --- . ."]
-morse_letter_space = "   "
-morse_word_space = "       "
+#Making the lists for the letters and morse code.
+alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "."]
+morse_characters_list = [". ---", "--- . . .", "--- . --- .", "--- . .", ".", ". . --- .", "--- --- .", ". . . .", ". .", ". --- --- ---", "--- . ---", ". --- . .", "--- ---", "--- .", "--- --- ---", ". --- --- .", "--- --- . ---", ". --- .", ". . .", "---", ". . ---", ". . . ---", ". --- ---", "--- . . ---", "--- . --- ---", "--- --- . .", ".-.-.-"]
 
-def english_to_morse():
+def word_to_morse(word):
+    morse_word = []
+    for character in word:
+        if character in alphabet_list:
+            character_index = alphabet_list.index(character)
+            morse_character = morse_characters_list[character_index]
+            morse_word.append(morse_character)
+    return " ".join(morse_word)
+
+    
+
+def english_to_morse(user_input):
+    morse_word_list = []
+    user_input = user_input.lower()
+    list_of_words = user_input.split()
+    for word in list_of_words:
+        morse_word_list.append((word_to_morse(word)))
+    return " / ".join(morse_word_list)
+
+    
+
+def morse_to_english(user_input):
     pass
 
-def morse_to_english():
-    pass
+sentence_1 = "Morse code is annoying."
+sentence_2 = "--- . --- . . --- --- .-.-.-"
