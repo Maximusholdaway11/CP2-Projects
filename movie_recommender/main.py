80f6b77b5eb7cb2e2eb00329ee8bdbf0fb5a1328
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-13 19:18:32 +0000

    Progress on move recommender

--- movie_recommender/main.py
+++ movie_recommender/main.py
@@ -23,3 +23,29 @@ def print_entire_movie_list(list_of_movies):
             print("")
             print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
+def movie_filters(list_of_movies, genre_decision, genre, rating_decision, rating):
+    if genre_decision.lower() == "yes":
+        if rating_decision.lower() == "no":
+            for dict in list_of_movies:
+                if dict['genre'] == genre:
+                    print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
+                    print("")
+    elif rating_decision.lower() == "yes":
+        if genre_decision.lower() == "no":
+            for dict in list_of_movies:
+                if dict['rating'] == rating:
+                    print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
+                    print("")
+    elif genre_decision.lower() == "yes" and rating_decision.lower() == "yes":
+        for dict in list_of_movies:
+            if dict['genre'] == genre and dict['rating'] == rating:
+                print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
+                print("")
+    else:
+        print("Unexpected error using filters please try again.")
+
+list_of_movies = get_movie_list(list_of_movies)
+
+print_entire_movie_list(list_of_movies)
+
+movie_filters(list_of_movies, "yes", "Drama", "no", "R")
