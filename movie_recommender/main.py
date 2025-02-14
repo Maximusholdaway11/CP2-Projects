884bd47ea3c022f78adc0c80e559f6f4451729d7
maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-14 10:51:01 -0700

    Filters suck

--- movie_recommender/main.py
+++ movie_recommender/main.py
@@ -3,13 +3,14 @@
 import csv
 
 list_of_movies = []
+list_of_filtered_movies = []
 
 def get_movie_list(list_of_movies):
     with open("movie_recommender/Movies list.csv") as csv_file:
         next(csv_file)
         csv_file = csv.reader(csv_file)
         for row in csv_file:
-            movie = {"title": row[0], "director": row[1], "genre": row[2], "rating": row[3]}
+            movie = {"title": row[0], "directors": row[1], "genre": row[2], "rating": row[3], "length" : row[4], "notable actors": row[5]}
             list_of_movies.append(movie)
         return list_of_movies
 
@@ -19,33 +20,21 @@ def print_entire_movie_list(list_of_movies):
     if list_of_movies is not []:
         for dict in list_of_movies:
             print("")
-            print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
+            print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['directors']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}, this is how long the movie is (in minutes): {dict['length']}, these are the notable actors: {dict['notable actors']}.")
             print("")
             print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
-def movie_filters(list_of_movies, genre_decision, genre, rating_decision, rating):
-    if genre_decision.lower() == "yes":
-        if rating_decision.lower() == "no":
-            for dict in list_of_movies:
-                if dict['genre'] == genre:
-                    print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
-                    print("")
-    elif rating_decision.lower() == "yes":
-        if genre_decision.lower() == "no":
-            for dict in list_of_movies:
-                if dict['rating'] == rating:
-                    print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
-                    print("")
-    elif genre_decision.lower() == "yes" and rating_decision.lower() == "yes":
-        for dict in list_of_movies:
-            if dict['genre'] == genre and dict['rating'] == rating:
-                print(f"This is the title of the movie: {dict['title']}, this is the director of the movie: {dict['director']} this is the genre: {dict['genre']}, this is the rating {dict['rating']}.")
-                print("")
-    else:
-        print("Unexpected error using filters please try again.")
+def title_filter(list_of_filtered_movies, title):
+    title.split(" ")
+
+    for dict in list_of_filtered_movies:
+        if dict['title'] in title:
+            list_of_filtered_movies.append(dict['title'])
+    return list_of_filtered_movies
 
 list_of_movies = get_movie_list(list_of_movies)
 
-print_entire_movie_list(list_of_movies)
+list_of_filtered_movies = get_movie_list(list_of_filtered_movies)
 
-movie_filters(list_of_movies, "yes", "Drama", "no", "R")
+list_of_filtered_movies = title_filter(list_of_filtered_movies, "Redemption")
+print(list_of_filtered_movies)
