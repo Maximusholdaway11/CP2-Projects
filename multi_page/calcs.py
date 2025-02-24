c0a3effa5969119a97fa6b0a11acfbc4c61aa8c6
maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-24 10:51:13 -0700

    Notes and ToDoList

---
+++ multi_page/calcs.py
@@ -0,0 +1,18 @@
+#Max Holdaway, multi page notes
+
+def add_numbers(num1, num2):
+    num3 = num1 + num2
+    return num3
+
+def subtract_numbers(num1, num2):
+    num3 = num1 - num2
+    return num3
+
+def divide_numbers(num1, num2):
+    num3 = num1 / num2
+    return num3
+
+def multiply_numbers(num1, num2):
+    num3 = num1 * num2
+    return num3
+
---
+++ multi_page/main.py
@@ -0,0 +1,5 @@
+#Max Holdaway, multi page notes
+
+from calcs import add_numbers as x
+
+print(x(4, 6))
