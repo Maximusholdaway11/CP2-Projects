e3662bc736f6c0143b61f873dd731ef5c8f88d18
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-03-17 10:51:39 -0600

    more UI stuff

--- battle_simulator/main.py
+++ battle_simulator/main.py
@@ -5,4 +5,15 @@ character_list = []
 
 def main(character_list):
     character_list = character_creator.load_characters()
-    
+    while True:
+        from InquirerPy import inquirer
+        from InquirerPy.base.control import Choice
+        from InquirerPy.separator import Separator
+        user_input = inquirer.select(
+            message="What do you want to do?:",
+            choices=[
+                "Create a character",
+                "Battle with characters",
+                "Exit"
+            ],
+        ).execute()
