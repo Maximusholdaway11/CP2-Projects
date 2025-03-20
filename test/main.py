8ba10f1738b9d89957bf635ac0736e8290fab79c
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-03-20 10:51:54 -0600

    Testing

--- test/main.py
+++ test/main.py
@@ -1,4 +1,18 @@
-var = "Comedy"
+import matplotlib.pyplot as plt
+import numpy as np
 
-if var in "Comedy/Drama":
-    print("Yes")
+plt.style.use('_mpl-gallery')
+
+x = 0.5 + np.arange(5)
+y = [5, 6, 4, 3, 2]
+
+graph_pos = [0.1, 0.1, 5, 5]
+
+labels = ["Health", "Strength", "Defense", "Speed", "xp"]
+
+fig, ax = plt.subplots()
+ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7, label=labels)
+ax.set(xlim=(0, 100), xticks=np.arange(1, 100),
+       ylim=(0, 100), yticks=np.arange(1, 100), xlabel="Type", ylabel="Stats", position=graph_pos)
+
+plt.show()
