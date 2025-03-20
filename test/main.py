eb5396d857575175073c36b4cc2bf82cfce3fee4
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-03-20 10:52:15 -0600

    Testing

--- test/main.py
+++ test/main.py
@@ -12,7 +12,7 @@ labels = ["Health", "Strength", "Defense", "Speed", "xp"]
 
 fig, ax = plt.subplots()
 ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7, label=labels)
-ax.set(xlim=(0, 100), xticks=np.arange(1, 100),
-       ylim=(0, 100), yticks=np.arange(1, 100), xlabel="Type", ylabel="Stats", position=graph_pos)
+ax.set(xlim=(0, 100), xticks=np.arange(1, 101),
+       ylim=(0, 100), yticks=np.arange(1, 101), xlabel="Type", ylabel="Stats", position=graph_pos)
 
 plt.show()
