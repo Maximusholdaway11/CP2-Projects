3b43bbbc3b20629dc8b22ab66b684023597ed517
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-03-25 10:51:49 -0600

    debugging / redoing assignment

--- battle_simulator/updated_battle_simulator/character_creator.py
+++ battle_simulator/updated_battle_simulator/character_creator.py
@@ -6,25 +6,26 @@ import copy
 from faker import Faker
 from InquirerPy import inquirer
 from InquirerPy.validator import NumberValidator
+import trace
 
 #Function to save characters to csv file
 def save_characters(characters):
-    with open("battle_simulator/characters.csv", "w") as file:
+    with open("battle_simulator/updated_battle_simulator/characters.csv", "w") as file:
         file_csv_writer = csv.writer(file)
         for character in characters:
             file_csv_writer.writerow(character.values())
 
 #Function to load characters from the csv file
 def load_characters(characters):
-    with open("battle_simulator/characters.csv", "r") as file:
+    with open("battle_simulator/updated_battle_simulator/characters.csv", "r") as file:
         file_csv_reader = csv.reader(file)
         for row in file_csv_reader:
-            if row[0] != "":
+            if row != []:
                 characters.append({"name": row[0], "health": int(row[1]), "strength": int(row[2]), "defense": int(row[3]), "speed": int(row[4]), "xp": int(row[5])})
         return characters
 
-#Function to display a characters information on a bar graph
-def display_character_info_bar_graph(user_char_input, characters):
+#Function to display a characters information
+def display_character_info(user_char_input, characters):
     temp_character = ""
     for character in characters:
         if character['name'] == user_char_input:
@@ -70,5 +71,6 @@ def character_creator(characters):
     character_backstory = word_generator.text()
     character_address = word_generator.address()
     user_character = {"name": user_char_name, "health": user_char_health, "strength": user_char_strength, "defense": user_char_defense, "speed": user_char_speed, "xp": 0, "backstory": character_backstory, "address": character_address}
+    print(f"The name of your character is: {user_char_name}")
     characters.append(user_character)
     return characters
