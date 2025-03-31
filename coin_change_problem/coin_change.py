3425b189da187784bb92f0ecc991239f04e57788
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-03-31 18:18:41 +0000

    Completed the counting coins part of coin change problem

--- coin_change_problem/coin_change.py
+++ coin_change_problem/coin_change.py
@@ -79,17 +79,19 @@ def load_coin_values(coin_value_list):
             elif row[0] == 'CH':
                 one_fen_split = row[1].split('-')
                 one_fen_value = {'coin_name': 'One Fen', 'coin_value': float(one_fen_split[1])}
-                five_fen_split = row[2].split('-')
-                five_fen_value = {'coin_name': 'Five Yen', 'coin_value': float(five_fen_split[1])}
-                one_jiao_split = row[3].split('-')
-                one_jiao_value = {'coin_name': 'Ten Yen', 'coin_value': float(one_jiao_split[1])}
-                two_jiao_split = row[4].split('-')
-                two_jiao_value ={'coin_name': 'Fifty Yen', 'coin_value': float(two_jiao_split[1])}
-                five_jiao_split = row[5].split('-')
-                five_jiao_value = {'coin_name': 'One Hundred Yen', 'coin_value': float(five_jiao_split[1])}
-                one_yuan_split = row[6].split('-')
-                one_yuan_value = {'coin_name': 'Five Hundred Yen', 'coin_value': float(one_yuan_split[1])}
-                ch_coin_values_dict = {'one_fen': one_fen_value, 'five_fen': five_fen_value, 'one_jiao': one_jiao_value, 'two_jiao': two_jiao_value, 'five_jiao': five_jiao_value, 'one_yuan': one_yuan_value, 'country': 'CH'}
+                two_fen_split = row[2].split('-')
+                two_fen_value = {'coin_name': 'Two Fen', 'coin_value': float(two_fen_split[1])}
+                five_fen_split = row[3].split('-')
+                five_fen_value = {'coin_name': 'Five Fen', 'coin_value': float(five_fen_split[1])}
+                one_jiao_split = row[4].split('-')
+                one_jiao_value = {'coin_name': 'One Jiao', 'coin_value': float(one_jiao_split[1])}
+                two_jiao_split = row[5].split('-')
+                two_jiao_value ={'coin_name': 'Two Jiao', 'coin_value': float(two_jiao_split[1])}
+                five_jiao_split = row[6].split('-')
+                five_jiao_value = {'coin_name': 'Five Jiao', 'coin_value': float(five_jiao_split[1])}
+                one_yuan_split = row[7].split('-')
+                one_yuan_value = {'coin_name': 'One Yuan', 'coin_value': float(one_yuan_split[1])}
+                ch_coin_values_dict = {'one_fen': one_fen_value, 'two_fen': two_fen_value, 'five_fen': five_fen_value, 'one_jiao': one_jiao_value, 'two_jiao': two_jiao_value, 'five_jiao': five_jiao_value, 'one_yuan': one_yuan_value, 'country': 'CH'}
                 coin_value_list.append(ch_coin_values_dict)
         return coin_value_list
 
@@ -125,15 +127,15 @@ def count_coins(coin_value_list):
             elif user_amount <= .09 and user_amount >= .05:
                 user_amount -= US_dict['nickel']
                 nickel_amount += 1
-            elif user_amount <= .04:
+            elif user_amount <= .04  and user_amount != 0:
                 user_amount -= US_dict['penny']
                 penny_amount += 1
         print(f'From your total amount {user_amount} there was:')
-        print(f'{dollar_coin_amount} many dollar coins.')
-        print(f'{quarter_amount} this many quarters.')
-        print(f'{dime_amount} this many dimes.')
-        print(f'{nickel_amount} this many nickels.')
-        print(f'{penny_amount} this many pennys.')
+        print(f'This many dollar coins: {dollar_coin_amount}')
+        print(f'This many quarters: {quarter_amount}')
+        print(f'This many dimes: {dime_amount}')
+        print(f'This many nickels: {nickel_amount}')
+        print(f'This many pennys: {penny_amount}')
     elif user_country == 'Europe':
         for country_value_dict in coin_value_list:
             if country_value_dict['country'] == 'EU':
@@ -153,7 +155,7 @@ def count_coins(coin_value_list):
                 user_amount -= EU_dict['two_euros']
                 two_euro_amount += 1
             elif user_amount <= 1.99 and user_amount >= 1:
-                user_amount -= EU_dict['one_euros']
+                user_amount -= EU_dict['one_euro']
                 one_euro_amount
             elif user_amount <= .99 and user_amount >= .50:
                 user_amount -= EU_dict['fifty_euro_cent']
@@ -170,15 +172,144 @@ def count_coins(coin_value_list):
             elif user_amount <= .04 and user_amount >= .02:
                 user_amount -= EU_dict['two_euro_cent']
                 two_euro_cent_amount += 1
-            elif user_amount < .02:
+            elif user_amount < .02  and user_amount != 0:
                 user_amount -= EU_dict['one_euro_cent']
                 one_euro_cent_amount += 1
         print(f'From your total amount {user_amount} there was:')
-        print(f'{two_euro_amount} this many two euro coins.')
-        print(f'{one_euro_amount} this many one euro coins.')
-        print(f'{fifty_euro_cent_amount} this many fifty euro cent coins.')
-        print(f'{twenty_euro_cent_amount} this many twenty euro cent coins.')
-        print(f'{ten_euro_cent_amount} this many ten euro cent coins.')
-        print(f'{five_euro_cent_amount} this many five euro cent coins.')
-        print(f'{two_euro_cent_amount} this many two euro cent coins.')
-        print(f'{one_euro_cent_amount} this many one euro cent coins.')
+        print(f'This many two euro coins: {two_euro_amount}')
+        print(f'This many one euro coins: {one_euro_amount}')
+        print(f'This many fifty euro cent coins: {fifty_euro_cent_amount}')
+        print(f'This many twenty euro cent coins: {twenty_euro_cent_amount}')
+        print(f'This many ten euro cent coins: {ten_euro_cent_amount}')
+        print(f'This many five euro cent coins: {five_euro_cent_amount}')
+        print(f'This many two euro cent coins: {two_euro_amount}')
+        print(f'This many one euro cent coins: {one_euro_cent_amount}')
+    elif user_country == 'Britain':
+        for country_value_dict in coin_value_list:
+            if country_value_dict['country'] == 'BR':
+                BR_dict = copy.deepcopy(country_value_dict)
+                coin_type = 'Pounds'
+        two_pound_amount = 0
+        one_pound_amount = 0
+        fifty_pence_amount = 0
+        twenty_pence_amount = 0
+        ten_pence_amount = 0
+        five_pence_amount = 0
+        two_pence_amount = 0
+        one_pence_amount = 0
+        user_amount = float(get_coin_amount(coin_type))
+        while user_amount > 0 and user_amount != 0:
+            if user_amount >= 2:
+                user_amount -= BR_dict['two_pounds']
+                two_pound_amount += 1
+            elif user_amount <= 1.99 and user_amount >= 1:
+                user_amount -= BR_dict['one_pound']
+                one_pound_amount
+            elif user_amount <= .99 and user_amount >= .50:
+                user_amount -= BR_dict['fifty_pence']
+                fifty_pence_amount += 1
+            elif user_amount <= .49 and user_amount >= .20:
+                user_amount -= BR_dict['twenty_pence']
+                twenty_pence_amount += 1
+            elif user_amount <= .19 and user_amount >= .10:
+                user_amount -= BR_dict['ten_pence']
+                ten_pence_amount += 1
+            elif user_amount <= .09 and user_amount >= .05:
+                user_amount -= BR_dict['five_pence']
+                five_pence_amount += 1
+            elif user_amount <= .04 and user_amount >= .02:
+                user_amount -= BR_dict['two_pence']
+                two_pence_amount += 1
+            elif user_amount < .02  and user_amount != 0:
+                user_amount -= BR_dict['one_pence']
+                one_pence_amount += 1
+        print(f'From your total amount {user_amount} there was:')
+        print(f'This many two pound coins: {two_pound_amount}')
+        print(f'This many one pound coins: {one_pound_amount}')
+        print(f'This many fifty pence coins: {fifty_pence_amount}')
+        print(f'This many twenty pence coins: {twenty_pence_amount}')
+        print(f'This many ten pence coins: {ten_pence_amount}')
+        print(f'This many five pence coins: {five_pence_amount}')
+        print(f'This many two pence coins: {two_pence_amount}')
+        print(f'This many one pence coins: {one_pence_amount}')
+    elif user_country == 'Japan':
+        for country_value_dict in coin_value_list:
+            if country_value_dict['country'] == 'JP':
+                JP_dict = copy.deepcopy(country_value_dict)
+                coin_type = 'Yens'
+        one_yen_amount = 0
+        five_yen_amount = 0
+        ten_yen_amount = 0
+        fifty_yen_amount = 0
+        one_hundred_yen_amount = 0
+        five_hundred_yen_amount = 0
+        user_amount = float(get_coin_amount(coin_type))
+        while user_amount > 0 and user_amount != 0:
+            if user_amount >= 500:
+                user_amount -= JP_dict['five_hundred_yen']
+                five_hundred_yen_amount += 1
+            elif user_amount <= 499 and user_amount >= 100:
+                user_amount -= JP_dict['hundred_yen']
+                one_hundred_yen_amount += 1
+            elif user_amount <= 99 and user_amount >= 50:
+                user_amount -= JP_dict['fifty_yen']
+                fifty_yen_amount += 1
+            elif user_amount <= 49 and user_amount >= 10:
+                user_amount -= JP_dict['ten_yen']
+                ten_yen_amount += 1
+            elif user_amount <= 9 and user_amount >= 5:
+                user_amount -= JP_dict['five_yen']
+                five_yen_amount += 1
+            elif user_amount < 5 and user_amount != 0:
+                user_amount -= JP_dict['one_yen']
+                one_yen_amount += 1
+        print(f'From your total amount {user_amount} there was:')
+        print(f'This many five hundred yen coins: {five_hundred_yen_amount}')
+        print(f'This many one hundred yen coins: {one_hundred_yen_amount}')
+        print(f'This many fifty yen coins: {fifty_yen_amount}')
+        print(f'This many ten yen coins: {ten_yen_amount}')
+        print(f'This many five yen coins: {five_yen_amount}')
+        print(f'This many one yen coins: {one_yen_amount}')
+    elif user_country == 'China':
+        for country_value_dict in coin_value_list:
+            if country_value_dict['country'] == 'CH':
+                CH_dict = copy.deepcopy(country_value_dict)
+                coin_type = 'Yuans'
+        one_fen_amount = 0
+        two_fen_amount = 0
+        five_fen_amount = 0
+        one_jiao_amount = 0
+        two_jiao_amount = 0
+        five_jiao_amount = 0
+        one_yuan_amount = 0
+        user_amount = float(get_coin_amount(coin_type))
+        while user_amount > 0 and user_amount != 0:
+            if user_amount >= 1:
+                user_amount -= CH_dict['one_yuan']
+                one_yuan_amount += 1
+            elif user_amount <= .99 and user_amount >= .50:
+                user_amount -= CH_dict['five_jiao']
+                five_jiao_amount += 1
+            elif user_amount <= .49 and user_amount >= .20:
+                user_amount -= CH_dict['two_jiao']
+                two_jiao_amount += 1
+            elif user_amount <= .19 and user_amount >= .10:
+                user_amount -= CH_dict['one_jiao']
+                one_jiao_amount += 1
+            elif user_amount <= .09 and user_amount >= .05:
+                user_amount -= CH_dict['five_fen']
+                five_fen_amount += 1
+            elif user_amount <= .04 >= .02:
+                user_amount -= CH_dict['two_fen']
+                two_fen_amount += 1
+            elif user_amount < .02 and user_amount != 0:
+                user_amount -= CH_dict['one_fen']
+                one_fen_amount += 1
+        print(f'From your total amount {user_amount} there was:')
+        print(f'This many one yuan coins: {one_yuan_amount}')
+        print(f'This many five jiao coins: {five_jiao_amount}')
+        print(f'This many two jiao coins: {two_jiao_amount}')
+        print(f'This many one jiao coins: {one_jiao_amount}')
+        print(f'This many five fen coins: {five_fen_amount}')
+        print(f'This many two fen coins: {two_fen_amount}')
+        print(f'This many one fen coins: {one_fen_amount}')
